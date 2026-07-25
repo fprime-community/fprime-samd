aab6a562339c066cb1c4087f87ec6b15a30af7bf
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2026-07-24 18:13:14 -0700

    Move config-samd to default/samd-config

--- fprime-samd/Svc/Framer/Framer.cpp
+++ fprime-samd/Svc/Framer/Framer.cpp
@@ -11,7 +11,7 @@
 #include "Svc/FprimeProtocol/FrameHeaderSerializableAc.hpp"
 #include "Svc/FprimeProtocol/FrameTrailerSerializableAc.hpp"
 #include "Utils/Hash/Hash.hpp"
-#include "config-samd/FramerConfig.hpp"
+#include "samd-config/FramerConfig.hpp"
 
 namespace Samd21 {
 
